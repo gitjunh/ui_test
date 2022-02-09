0ed06dc3a006aa5f8ef441815bff3d9198a1137c
junhknewit <junhknewit@gmail.com>
2022-02-10 00:49:54 +0900

    Bottom navigation

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:animations/animations.dart';
 
 void main() => runApp(const MyApp());
 
@@ -13,7 +14,9 @@ class MyApp extends StatelessWidget {
       title: _title,
       // home: MyStatelessWidget(),
       // home: BottomAppBarDemo(),
-      home: BottomNavigationDemo('', '', BottomNavigationDemoType.withLabels),
+      home: BottomNavigationDemo(
+          restorationId: 'bottom_navigation_labels_demo',
+          type: BottomNavigationDemoType.withLabels),
     );
   }
 }
@@ -24,9 +27,10 @@ enum BottomNavigationDemoType {
 }
 
 class BottomNavigationDemo extends StatefulWidget {
-  const BottomNavigationDemo({Key? key,
-  @required this.restorationId,
-  @required this.type,
+  const BottomNavigationDemo({
+    Key? key,
+    @required this.restorationId,
+    @required this.type,
   }) : super(key: key);
 
   final String? restorationId;
@@ -36,8 +40,8 @@ class BottomNavigationDemo extends StatefulWidget {
   _BottomNavigationDemoState createState() => _BottomNavigationDemoState();
 }
 
-class _BottomNavigationDemoState extends State<BottomNavigationDemo> with RestorationMixin{
-
+class _BottomNavigationDemoState extends State<BottomNavigationDemo>
+    with RestorationMixin {
   final RestorableInt _currentIndex = RestorableInt(0);
 
   @override
@@ -55,18 +59,131 @@ class _BottomNavigationDemoState extends State<BottomNavigationDemo> with Restor
   }
 
   String _title(BuildContext context) {
-    switch(widget.type) {
+    switch (widget.type) {
       case BottomNavigationDemoType.withLabels:
-        return '지속적 라벨'
+        return '지속적 라벨';
+      case BottomNavigationDemoType.withoutLabels:
+        return '선택한 라벨';
     }
+    return '';
   }
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    final colorScheme = Theme.of(context).colorScheme;
+    final textTheme = Theme.of(context).textTheme;
+
+    var bottomNavigationBarItems = <BottomNavigationBarItem>[
+      const BottomNavigationBarItem(
+        icon: Icon(Icons.add_comment),
+        label: '댓글',
+      ),
+      const BottomNavigationBarItem(
+        icon: Icon(Icons.calendar_today),
+        label: '캘린더',
+      ),
+      const BottomNavigationBarItem(
+        icon: Icon(Icons.account_circle),
+        label: '계정',
+      ),
+      const BottomNavigationBarItem(
+        icon: Icon(Icons.alarm_on),
+        label: '알람',
+      ),
+      const BottomNavigationBarItem(
+        icon: Icon(Icons.camera_enhance),
+        label: '카메라',
+      ),
+    ];
+
+    if (widget.type == BottomNavigationDemoType.withLabels) {
+      bottomNavigationBarItems = bottomNavigationBarItems.sublist(
+          0, bottomNavigationBarItems.length - 2);
+      _currentIndex.value = _currentIndex.value
+          .clamp(0, bottomNavigationBarItems.length - 1)
+          .toInt();
+    }
+    return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        title: Text(_title(context)),
+      ),
+      body: Center(
+        child: PageTransitionSwitcher(
+          transitionBuilder: (child, animation, secondaryAnimation) {
+            return FadeThroughTransition(
+              animation: animation,
+              secondaryAnimation: secondaryAnimation,
+              child: child,
+            );
+          },
+          child: _NavigaionDestinationView(
+            key: UniqueKey(),
+            item: bottomNavigationBarItems[_currentIndex.value],
+          ),
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        showUnselectedLabels:
+        widget.type == BottomNavigationDemoType.withLabels,
+        items: bottomNavigationBarItems,
+        currentIndex: _currentIndex.value,
+        type: BottomNavigationBarType.fixed,
+        // selectedFontSize: textTheme.caption.fontSize,
+        // unselectedFontSize: textTheme.caption.fontSize,
+        onTap: (index) {
+          setState((){
+            _currentIndex.value = index;
+          });
+        },
+        selectedItemColor: colorScheme.onPrimary,
+        unselectedItemColor: colorScheme.onPrimary.withOpacity(0.38),
+        backgroundColor: colorScheme.primary,
+      ),
+    );
   }
 }
 
+class _NavigaionDestinationView extends StatelessWidget {
+  const _NavigaionDestinationView({Key? key, required this.item})
+      : super(key: key);
+
+  final BottomNavigationBarItem item;
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        ExcludeSemantics(
+          child: Center(
+            child: Padding(
+              padding: const EdgeInsets.all(16),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(8),
+                child: Image.asset(
+                  'assets/demos/bottom_navigation_background.png',
+                  package: 'flutter_gallery_assets',
+                ),
+              ),
+            ),
+          ),
+        ),
+        Center(
+          child: IconTheme(
+            data: const IconThemeData(
+              color: Colors.white,
+              size: 80,
+            ),
+            child: Semantics(
+              label: 'aaa-aaa',
+              child: item.icon,
+            ),
+          ),
+        )
+      ],
+    );
+  }
+}
 
 // class BottomAppBarDemo extends StatefulWidget {
 //   const BottomAppBarDemo({Key? key}) : super(key: key);
